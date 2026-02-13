888e70c929f01d813bb60b544f8938dc06ae775a
raiyanamiruddin <raiyan.amiruddin@gmail.com>
2026-02-12 19:53:24 -0500

    Added objects to the main

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -3,6 +3,10 @@
  */
 public class TopSecret {
     public static void main(String[] args) {
+        FileHandler fileHandler = new FileHandler();
+        CommandLineInterface cli = new CommandLineInterface();
+        ProgramControl pc = new ProgramControl(cli, fileHandler);
+        pc.run(args);
 
     }
 }
